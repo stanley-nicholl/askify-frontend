5a405a30d930f050e1c56bfc457e9ae75e8a0dd9
Kat Reinhart <kat.reinhart@gmail.com>
2018-01-16 10:31:34 -0800

    Convert QuestionItem to class component with state

--- src/components/commonComponents/QuestionItem.js
+++ src/components/commonComponents/QuestionItem.js
@@ -1,42 +1,59 @@
-import React from 'react'
+import React, { Component } from 'react'
 
-const QuestionItem = ({ id, count, name, question, updateQueueOrder, currentUserId, questionUserId, currentQueueOrder }) => {
-    let styling = 'd-flex row question-item py-3'
-    let disabled = 'false'
-
-    if(currentUserId === questionUserId) {
-      styling = 'd-flex row question-item py-3 active-item'
-    }else{
-      disabled = 'true'
+class QuestionItem extends Component {
+  
+  constructor(props) {
+    super(props)
+    this.state = {
+      styling: 'd-flex row question-item py-3',
+      editing: false,
+      disabled: 'false'
     }
+  }
 
-    if(count !== currentQueueOrder && currentUserId === questionUserId){
-      updateQueueOrder(count)
-    }
+  editQuestion = (e) => {
+    e.preventDefault()
+    this.setState({
+      ...this.state,
+      editing: !this.state.editing
+    })
+  }
 
-    const editQuestion = (e) => {
-      e.preventDefault()
+  buttonId = (role) => {
+    return this.props.id + "-" + role
+  }
 
-    }
+  renderQuestionText = () => {
+    if (this.state.editing) {
+      return (
+        <div className="col-6 d-flex align-items-center">
+          <input type="text" className="element topic" id="${id}-question" value={this.props.question} />
+        </div>
+      )
+    } else return (
+      <div className="col-6 d-flex align-items-center">
+        <p className="element topic" id="${id}-question">{this.props.question}</p>
+      </div>
+    )
+  }
 
-
+  render() { 
     return (
-      <div className={styling}>
+      <div className={this.state.styling}>
         <div className="col-1 d-flex justify-content-center align-items-center">
-          <p className="element queueNum">{count}</p>
+          <p className="element queueNum">{this.props.count}</p>
         </div>
         <div className="col-3 d-flex align-items-center">
-          <p className="element name">{name}</p>
+          <p className="element name">{this.props.name}</p>
         </div>
-        <div className="col-6 d-flex align-items-center">
-          <p className="element topic" id="${id}-question">{question}</p>
-        </div>
+        { this.renderQuestionText() }
         <div className="col-2 d-flex flex-column align-items-center justify-content-center">
-          <button type="button" id="${id}-edit" className='btn btn-warning waves-effect btn-sm item-button mt-2' disabled={disabled} onClick={e => editQuestion()}>Edit</button>
-          <button type="button" id="${id}-answered" className='btn waves-effect btn-sm item-button mt-2 answered-btn' disabled={disabled}>Answered</button>
+          <button type="button" id={this.buttonId('edit')} className='btn btn-warning waves-effect btn-sm item-button mt-2' onClick={e => this.editQuestion(e)}>Edit</button>
+          <button type="button" id={this.buttonId('answered')} className='btn waves-effect btn-sm item-button mt-2 answered-btn' disabled={this.state.disabled}>Answered</button>
         </div>
       </div>
     )
+  }
 }
-
+ 
 export default QuestionItem
